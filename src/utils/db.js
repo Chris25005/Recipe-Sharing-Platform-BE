f6aac8b45254e6486014a7d36b2e173ec1e31be3
Chris25005 <chriswakar@gamil.com>
2025-11-04 15:46:29 +0530

    all done

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 export async function connectToDatabase() {
-  const mongoUri = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/recipeweb';
+  const mongoUri = process.env.MONGODB_URI || 'mongodb+srv://chrisdiva07:Mongo%40123@cluster007.6v15lqr.mongodb.net/';
   mongoose.set('strictQuery', true);
   await mongoose.connect(mongoUri, {
     autoIndex: true
